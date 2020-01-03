4d010618f482e18001c30cc0050049cb761aadb9
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-01-03 13:35:39 -0500

    repositorios necesarios

--- src/main/java/service/Servicio.java
+++ src/main/java/service/Servicio.java
@@ -1,5 +1,7 @@
 package service;
 
+import java.util.Date;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -8,6 +10,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import dao.UsuarioDAO;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import modelo.Usuario;
 
 @Path("/auth")
@@ -17,10 +21,7 @@ public class Servicio {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response validar(Usuario usuario) {
-		boolean s = UsuarioDAO.validar(usuario);
-		if(s) {
-			
-		}
+		
 		return Response.status(Response.Status.UNAUTHORIZED).build();
 	}
 	
