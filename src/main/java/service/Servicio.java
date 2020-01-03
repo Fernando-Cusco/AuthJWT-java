5c8761c6fac073c67c4dec2b5cabeaffdeeda5eb
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-01-03 13:53:17 -0500

    servicio post

--- src/main/java/service/Servicio.java
+++ src/main/java/service/Servicio.java
@@ -25,7 +25,22 @@ public class Servicio {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response validar(Usuario usuario) {
+		boolean s = UsuarioDAO.validar(usuario);
+		if(s) {
+			long tiempo = System.currentTimeMillis();
+			String key = "password";
+			String jwt = Jwts.builder()
+					.signWith(SignatureAlgorithm.HS256, key)
+					.setSubject("Fernando Cusco")
+					.setIssuedAt(new Date(tiempo))
+					.setExpiration(new Date(tiempo+900000))
+					.claim("email", "admin@admin.com")
+					.compact();
 		
+			JsonObject json = Json.createObjectBuilder()
+								.add("JWT", jwt).build();
+			return Response.status(Response.Status.CREATED).entity(json).build();
+		}
 		return Response.status(Response.Status.UNAUTHORIZED).build();
 	}
 	
