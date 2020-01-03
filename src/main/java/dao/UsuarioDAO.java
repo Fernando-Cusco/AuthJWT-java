8367f852561bcb966cb1c6d59f80489763da0c39
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-01-03 13:18:50 -0500

    usuarioDao para validad

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -1,5 +1,11 @@
 package dao;
 
+import modelo.Usuario;
+
 public class UsuarioDAO {
+	
+	public boolean validar(Usuario usuario) {
+		return (usuario.getUsuario().equals("admin") && usuario.getPassword().equals("admin"));
+	}
 
 }
